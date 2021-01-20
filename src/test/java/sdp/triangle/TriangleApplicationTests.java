fa85f2ac3022b9ec450b2e7f312d9790ff477301
Bartłomiej Mazurek <mazurekb02@gmail.com>
2021-01-20 11:28:18 +0100

    init commit

---
+++ src/test/java/sdp/triangle/TriangleApplicationTests.java
@@ -0,0 +1,13 @@
+package sdp.triangle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriangleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
